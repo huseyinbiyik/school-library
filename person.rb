824ad5286492a45d46e4d6b9f4075d3dc57eb613
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-08-02 09:51:05 +0300

    Add conditional methods to person class

--- person.rb
+++ person.rb
@@ -1,15 +1,19 @@
 class Person
-  def initialize (name = "Unknown", age, parent_permission = true)
-    @id
+  def initialize (name = "Unknown", age, parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  attr_reader :id
-  attr_reader :name
-  attr_reader :age
+  attr_accessor(:age, :name)
+  attr_reader(:id)
 
-  attr_writer :name
-  attr_writer :age
+  private def is_of_age?
+    if @age >= 18
+    end
+
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
 end
