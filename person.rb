c3b1e4d0d4dd138d5ed1dd18df4121934166c801
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-08-03 09:04:24 +0300

    Remove test code

--- person.rb
+++ person.rb
@@ -27,11 +27,3 @@ class Person < Nameable
     of_age? || parent_permission
   end
 end
-
-person = Person.new(22, 'maximilianus')
-puts person
-puts person.correct_name
-capitalizedPerson = CapitalizeDecorator.new(person)
-puts capitalizedPerson.correct_name
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-puts capitalizedTrimmedPerson.correct_name
