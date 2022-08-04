7f34b12f297b9390486406a2c610e3f60e830e76
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-08-04 13:21:16 +0300

    Add a rental belongs to a book relationship

--- book.rb
+++ book.rb
@@ -1,3 +1,4 @@
+require_relative 'rental'
 class Book
   attr_accessor :title, :author
 
--- rental.rb
+++ rental.rb
@@ -1,7 +1,15 @@
+require_relative 'book'
 class Rental
   attr_accessor :date
+  attr_reader :book
 
   def initialize(date)
     @date = date
+    @book = book
+  end
+
+  def book=(book)
+    @book = book
+    book.rentals.push(self) unless book.rentals.include?(self)
   end
 end
