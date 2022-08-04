3b670da7125b969b3d554a025f8d83c68743b2b2
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-08-04 23:21:32 +0300

    Add create student method

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require_relative 'book'
+require_relative 'student'
 class App
+  attr_reader :people
+
   def initialize
     @books = []
+    @people = []
   end
 
   def menu
@@ -19,6 +23,8 @@ class App
     case answer
     when 1
       list_books
+    when 3
+      create_person
     else
       puts 'Wrong option'
     end
@@ -34,6 +40,28 @@ class App
     puts 'Listing all the books in the library:'
     @books.each do |book|
       puts "#{book.title} by #{book.author}"
+    end
+  end
+
+  def create_person
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    answer = gets.chomp.to_i
+    if answer == 1
+      puts 'Age:'
+      age = gets.chomp.to_i
+      puts 'Name:'
+      name = gets.chomp.to_i
+      puts 'Has parent permission? [Y/N]'
+      permission = gets.chomp.upcase
+      case permission
+      when 'Y'
+        permission = true
+      when 'N'
+        permission = false
+      end
+      student = Student.new(nil, age, name, parent_permission: permission)
+      @people.push(student)
+      puts 'A student created successfully'
     end
   end
 end
