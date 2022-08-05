ad36997bc0f41573832dd562466fca6ea7d30e7e
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-08-05 07:26:37 +0300

    Add create new teacher method

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require_relative 'book'
 require_relative 'student'
+require_relative 'teacher'
 class App
   attr_reader :people
 
@@ -46,22 +47,58 @@ class App
   def create_person
     puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     answer = gets.chomp.to_i
-    if answer == 1
-      puts 'Age:'
-      age = gets.chomp.to_i
-      puts 'Name:'
-      name = gets.chomp.to_i
-      puts 'Has parent permission? [Y/N]'
-      permission = gets.chomp.upcase
-      case permission
-      when 'Y'
-        permission = true
-      when 'N'
-        permission = false
-      end
-      student = Student.new(nil, age, name, parent_permission: permission)
-      @people.push(student)
-      puts 'A student created successfully'
+
+    case answer
+    when 1
+      create_student
+    when 2
+      create_teacher
     end
+
+    run
+  end
+
+  def create_student
+    print 'Age:'
+    age = gets.chomp.to_i
+    print 'Name:'
+    name = gets.chomp.to_i
+
+    print 'Has parent permission? [Y/N]'
+    permission = gets.chomp.upcase
+    case permission
+    when 'Y'
+      permission = true
+    when 'N'
+      permission = false
+    end
+
+    student = Student.new(nil, age, name, parent_permission: permission)
+    @people.push(student)
+    puts 'A student created successfully'
+    puts ''
+  end
+
+  def create_teacher
+    print 'Age:'
+    age = gets.chomp.to_i
+    print 'Specialization:'
+    specialization = gets.chomp
+    print 'Name:'
+    name = gets.chomp
+
+    print 'Has parent permission? [Y/N]'
+    permission = gets.chomp.upcase
+    case permission
+    when 'Y'
+      permission = true
+    when 'N'
+      permission = false
+    end
+
+    teacher = Teacher.new(age, specialization, name, parent_permission: permission)
+    @people.push(teacher)
+    puts 'A teacher created successfully'
+    puts ''
   end
 end
